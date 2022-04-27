e295812b8bb82a45be69b127075ed50eeb491c68
Peter <schmidt.multimedia@gmail.com>
2022-04-27 09:32:25 +0200

    AL deklarieren / füllen/ lesen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,31 @@
 package com.cc.java;
 
+import java.util.ArrayList;
+
 public class App {
     
     public static void main(String[] args) {
      
+      	// ArrayList (AL) anlegen ...
+        ArrayList<String> carMakers = new ArrayList<>();
+
+        output("AL: " + carMakers);
+        output("Elementenanzahl: " + carMakers.size());
+
+        // AL dynamisch füllen (JS --> push() / J --> add())
+        carMakers.add("Volvo");
+		carMakers.add("BMW");
+		carMakers.add("Ford");
+		carMakers.add("Honda");
+		carMakers.add("Tesla");
+
+        output("AL: " + carMakers);
+        output("Elementenanzahl: " + carMakers.size());
+        
+        // Daten aus AL lesen
+        output("1. Element: " + carMakers.get(0));
+        output("Letztes Element: " + carMakers.get(carMakers.size()-1));
+
     }
 
     public static void output(String outputStr) {
